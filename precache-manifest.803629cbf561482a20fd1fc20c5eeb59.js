632b41cf22407439bd1ebbb88fdb2ef692aeecf7
Harpanth <panthdhillon6@gmail.com>
2021-12-19 20:51:39 +0530

    Updates

---
+++ precache-manifest.803629cbf561482a20fd1fc20c5eeb59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f205be6c4ff227d6d1b8309b365422c",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "e10ce4aec9b0aaa1f04e",
+    "url": "/Portfolio/static/css/main.d0acee29.chunk.css"
+  },
+  {
+    "revision": "bca5935f3b2fad1c0b1c",
+    "url": "/Portfolio/static/js/2.d3ff1cbf.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/Portfolio/static/js/2.d3ff1cbf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e10ce4aec9b0aaa1f04e",
+    "url": "/Portfolio/static/js/main.b70e1f72.chunk.js"
+  },
+  {
+    "revision": "42239e215735ecc9d792",
+    "url": "/Portfolio/static/js/runtime-main.c215444c.js"
+  },
+  {
+    "revision": "c7391e6ebad0253dd47db438776ed492",
+    "url": "/Portfolio/static/media/aa.c7391e6e.png"
+  }
+]);
